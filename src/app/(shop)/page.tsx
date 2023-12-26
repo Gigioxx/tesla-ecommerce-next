d3f766812ae00631f2f26f75f0d3c6e63b4bcc84
Gigiox <guillermo.casanova.b@gmail.com>
2023-12-26 15:47:55 -0300

    feat: display products in home page

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,14 @@
-import { Title } from '@/components';
+import { ProductGrid, Title } from '@/components';
+import { initialData } from '@/seed/seed';
+
+const products = initialData.products;
 
 export default function Home() {
   return (
     <>
       <Title className='mb-2' subtitle='All products' title='Store' />
+
+      <ProductGrid products={products} />
     </>
   );
 }
